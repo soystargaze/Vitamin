f7106e9771febfd637abf2831e5299d6bbb17fc4
stargaze <mail@erosmari.com>
2025-02-18 18:03:22 +0100

    updated settings.gradle

--- src/main/java/com/erosmari/vitamin/utils/TranslationHandler.java
+++ src/main/java/com/erosmari/vitamin/utils/TranslationHandler.java
@@ -85,7 +85,7 @@ public class TranslationHandler {
     }
 
     public static Component getPlayerMessage(String key, Object... args) {
-        String prefix = translations.getOrDefault("plugin.prefix", "<gray>[</gray><gradient:#FFA500:#FFFF00>Vitamin</gradient><color:#FFA500>+</color><gray>]</gray> ");
+        String prefix = translations.getOrDefault("plugin.prefix", "[<gradient:#FFA500:#FFFF00>Vitamin</gradient><color:#FFA500>+</color>] ");
         String dynamicColor = translations.getOrDefault("plugin.dynamic_color", "<color:#FFA500>");
         String template = translations.getOrDefault(key, "Translation not found: " + key + "!");
 
@@ -108,7 +108,7 @@ public class TranslationHandler {
     }
 
     public static Component getLogMessage(String key, Object... args) {
-        String prefix = translations.getOrDefault("plugin.prefix", "<gray>[</gray><gradient:#FFA500:#FFFF00:#FFFF00>Vitamin</gradient><gold>+</gold><gray>]</gray> ");
+        String prefix = translations.getOrDefault("plugin.prefix", "[<gradient:#FFA500:#FFFF00:#FFFF00>Vitamin</gradient><gold>+</gold>] ");
         String template = translations.getOrDefault(key, "Translation not found: " + key + "!");
         String dynamicColor = translations.getOrDefault("plugin.dynamic_color", "<color:#FFA500>");
 
