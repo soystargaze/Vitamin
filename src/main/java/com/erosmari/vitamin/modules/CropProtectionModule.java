5faa9e024285caea0a41186604083612f70cf1e1
stargaze <mail@erosmari.com>
2025-02-26 20:42:43 +0100

    added new module

--- src/main/java/com/erosmari/vitamin/modules/CropProtectionModule.java
+++ src/main/java/com/erosmari/vitamin/modules/CropProtectionModule.java
@@ -1,6 +1,8 @@
 package com.erosmari.vitamin.modules;
 
+import com.erosmari.vitamin.database.DatabaseHandler;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -12,6 +14,11 @@ public class CropProtectionModule implements Listener {
     public void onPlayerInteract(PlayerInteractEvent event) {
         if (event.getAction() == Action.PHYSICAL) {
             if (event.getClickedBlock() != null) {
+                Player player = event.getPlayer();
+                if (!player.hasPermission("vitamin.module.crop_protection") ||
+                        !DatabaseHandler.isModuleEnabledForPlayer(player.getUniqueId(), "module.crop_protection")) {
+                    return;
+                }
                 Material type = event.getClickedBlock().getType();
                 if (isCrop(type) || type == Material.FARMLAND) {
                     event.setCancelled(true);
@@ -22,7 +29,7 @@ public class CropProtectionModule implements Listener {
 
     private boolean isCrop(Material material) {
         return switch (material) {
-            case WHEAT, CARROTS, POTATOES, BEETROOTS -> true;
+            case WHEAT, CARROTS, POTATOES, BEETROOTS, PUMPKIN, MELON -> true;
             default -> false;
         };
     }
