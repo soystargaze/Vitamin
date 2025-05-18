bcaeb8d72def9c0fd27f310683d82b3116639c2d
stargaze <mail@erosmari.com>
2025-05-18 19:53:33 +0200

    v1.4.4 fixed repair module, ReplayableVaultModule, ja_jp.yml

--- src/main/java/com/soystargaze/vitamin/modules/core/VeinMinerModule.java
+++ src/main/java/com/soystargaze/vitamin/modules/core/VeinMinerModule.java
@@ -4,6 +4,8 @@ import com.soystargaze.vitamin.database.DatabaseHandler;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.ExperienceOrb;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,6 +21,8 @@ public class VeinMinerModule implements Listener {
 
     private static final Set<Material> ORES = EnumSet.noneOf(Material.class);
     private static final Map<Material, Material> RAW_TO_INGOT = new EnumMap<>(Material.class);
+    private static final Set<Material> XP_ORES = EnumSet.noneOf(Material.class);
+    private static final Random RANDOM = new Random();
 
     static {
         ORES.add(Material.COAL_ORE);
@@ -43,6 +47,21 @@ public class VeinMinerModule implements Listener {
         RAW_TO_INGOT.put(Material.RAW_IRON, Material.IRON_INGOT);
         RAW_TO_INGOT.put(Material.RAW_GOLD, Material.GOLD_INGOT);
         RAW_TO_INGOT.put(Material.RAW_COPPER, Material.COPPER_INGOT);
+
+        XP_ORES.add(Material.COAL_ORE);
+        XP_ORES.add(Material.DEEPSLATE_COAL_ORE);
+        XP_ORES.add(Material.COPPER_ORE);
+        XP_ORES.add(Material.DEEPSLATE_COPPER_ORE);
+        XP_ORES.add(Material.DIAMOND_ORE);
+        XP_ORES.add(Material.DEEPSLATE_DIAMOND_ORE);
+        XP_ORES.add(Material.EMERALD_ORE);
+        XP_ORES.add(Material.DEEPSLATE_EMERALD_ORE);
+        XP_ORES.add(Material.LAPIS_ORE);
+        XP_ORES.add(Material.DEEPSLATE_LAPIS_ORE);
+        XP_ORES.add(Material.REDSTONE_ORE);
+        XP_ORES.add(Material.DEEPSLATE_REDSTONE_ORE);
+        XP_ORES.add(Material.NETHER_QUARTZ_ORE);
+        XP_ORES.add(Material.NETHER_GOLD_ORE);
     }
 
     public VeinMinerModule(JavaPlugin ignored) {
@@ -114,6 +133,26 @@ public class VeinMinerModule implements Listener {
         for (ItemStack drop : drops) {
             ore.getWorld().dropItemNaturally(ore.getLocation(), drop);
         }
+
+        if (!hasSilkTouch && XP_ORES.contains(ore.getType())) {
+            int[] range = getExperienceRange(ore.getType());
+            int exp = range[0] + RANDOM.nextInt(range[1] - range[0] + 1);
+            if (exp > 0) {
+                ExperienceOrb orb = (ExperienceOrb) ore.getWorld().spawnEntity(ore.getLocation(), EntityType.EXPERIENCE_ORB);
+                orb.setExperience(exp);
+            }
+        }
+    }
+
+    private int[] getExperienceRange(Material oreType) {
+        return switch (oreType) {
+            case COAL_ORE, DEEPSLATE_COAL_ORE, COPPER_ORE, DEEPSLATE_COPPER_ORE -> new int[]{0, 2};
+            case DIAMOND_ORE, DEEPSLATE_DIAMOND_ORE, EMERALD_ORE, DEEPSLATE_EMERALD_ORE -> new int[]{3, 7};
+            case LAPIS_ORE, DEEPSLATE_LAPIS_ORE, NETHER_QUARTZ_ORE -> new int[]{2, 5};
+            case REDSTONE_ORE, DEEPSLATE_REDSTONE_ORE -> new int[]{1, 5};
+            case NETHER_GOLD_ORE -> new int[]{0, 1};
+            default -> new int[]{0, 0};
+        };
     }
 
     private void findConnectedOres(Block block, Material oreType, Set<Block> visited) {
