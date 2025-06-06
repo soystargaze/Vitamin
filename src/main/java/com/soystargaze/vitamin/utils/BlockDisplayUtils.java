70c80e682add5e0ddc0041688b9a891c4c55fd2d
stargaze <mail@erosmari.com>
2025-06-07 01:10:26 +0200

    v1.6.0, waystone, wg flags, gp fix, CarryOnModule fix, console commands, Restore command and carryOn registry file, Clear Command
    TODO more themes

--- src/main/java/com/soystargaze/vitamin/utils/BlockDisplayUtils.java
+++ src/main/java/com/soystargaze/vitamin/utils/BlockDisplayUtils.java
@@ -17,13 +17,58 @@ public class BlockDisplayUtils {
 
     static {
         MATERIAL_THEMES.put(Material.STONE, Arrays.asList(
-                Material.STONE_BRICKS,      // 0 - project_0
-                Material.SMOOTH_STONE,      // 1 - project_1
-                Material.STONE_BRICKS,      // 2 - project_2
-                Material.STONE_BRICKS,      // 3 - project_3
-                Material.STONE_BRICKS,      // 4 - project_4
-                Material.SMOOTH_STONE,      // 5 - project_5
-                Material.LODESTONE          // 6 - project_6
+                Material.STONE_BRICKS,                      // 0 - project_0
+                Material.SMOOTH_STONE,                      // 1 - project_1
+                Material.STONE_BRICKS,                      // 2 - project_2
+                Material.STONE_BRICKS,                      // 3 - project_3
+                Material.STONE_BRICKS,                      // 4 - project_4
+                Material.SMOOTH_STONE,                      // 5 - project_5
+                Material.CHISELED_STONE_BRICKS              // 6 - project_6
+        ));
+        MATERIAL_THEMES.put(Material.SANDSTONE, Arrays.asList(
+                Material.CUT_RED_SANDSTONE,                  // 0 - Base inferior
+                Material.SMOOTH_RED_SANDSTONE,               // 1 - Pilar superior
+                Material.SMOOTH_SANDSTONE,                   // 2 - Marco inferior
+                Material.CUT_RED_SANDSTONE,                  // 3 - Base superior
+                Material.SMOOTH_SANDSTONE,                   // 4 - Marco superior
+                Material.SMOOTH_RED_SANDSTONE,               // 5 - Pilar inferior
+                Material.CHISELED_SANDSTONE                  // 6 - Centro/núcleo
+        ));
+        MATERIAL_THEMES.put(Material.RED_SANDSTONE, Arrays.asList(
+                Material.CUT_SANDSTONE,
+                Material.SMOOTH_SANDSTONE,
+                Material.SMOOTH_RED_SANDSTONE,
+                Material.CUT_SANDSTONE,
+                Material.SMOOTH_RED_SANDSTONE,
+                Material.SMOOTH_SANDSTONE,
+                Material.CHISELED_RED_SANDSTONE
+        ));
+        MATERIAL_THEMES.put(Material.BLACKSTONE, Arrays.asList(
+                Material.POLISHED_BLACKSTONE_BRICKS,
+                Material.GILDED_BLACKSTONE,
+                Material.NETHERITE_BLOCK,
+                Material.POLISHED_BLACKSTONE_BRICKS,
+                Material.NETHERITE_BLOCK,
+                Material.GILDED_BLACKSTONE,
+                Material.CHISELED_POLISHED_BLACKSTONE
+        ));
+        MATERIAL_THEMES.put(Material.NETHERRACK, Arrays.asList(
+                Material.NETHER_BRICKS,
+                Material.MAGMA_BLOCK,
+                Material.NETHER_BRICKS,
+                Material.NETHER_BRICKS,
+                Material.NETHER_BRICKS,
+                Material.MAGMA_BLOCK,
+                Material.CHISELED_NETHER_BRICKS
+        ));
+        MATERIAL_THEMES.put(Material.RESIN_BLOCK, Arrays.asList(
+                Material.RESIN_BRICKS,
+                Material.HONEYCOMB_BLOCK,
+                Material.RESIN_BRICKS,
+                Material.RESIN_BRICKS,
+                Material.RESIN_BRICKS,
+                Material.HONEYCOMB_BLOCK,
+                Material.CHISELED_RESIN_BRICKS
         ));
     }
 
@@ -37,25 +82,12 @@ public class BlockDisplayUtils {
 
         List<BlockDisplay> displays = new ArrayList<>();
 
-        // project_0: Base inferior
         displays.add(createBlockDisplay(baseLoc, themeMaterials.get(0), 1f, 0.25f, 1f, 0f, 0f, 0f));
-
-        // project_1: Pilar superior
         displays.add(createBlockDisplay(baseLoc, themeMaterials.get(1), 0.625f, 0.9375f, 0.625f, 0.1875f, 1f, 0.1875f));
-
-        // project_2: Marco inferior
         displays.add(createBlockDisplay(baseLoc, themeMaterials.get(2), 0.875f, 0.1875f, 0.875f, 0.0625f, 0.25f, 0.0625f));
-
-        // project_3: Base superior
         displays.add(createBlockDisplay(baseLoc, themeMaterials.get(3), 1f, 0.25f, 1f, 0f, 1.75f, 0f));
-
-        // project_4: Marco superior
         displays.add(createBlockDisplay(baseLoc, themeMaterials.get(4), 0.875f, 0.1875f, 0.875f, 0.0625f, 1.5625f, 0.0625f));
-
-        // project_5: Pilar inferior
         displays.add(createBlockDisplay(baseLoc, themeMaterials.get(5), 0.625f, 0.9375f, 0.625f, 0.1875f, 0.0625f, 0.1875f));
-
-        // project_6: Centro/núcleo (lodestone)
         displays.add(createBlockDisplay(baseLoc, themeMaterials.get(6), 0.75f, 0.75f, 0.75f, 0.125f, 0.625f, 0.125f));
 
         return displays;
